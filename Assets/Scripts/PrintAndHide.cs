aefd4087cb278e7b1e46c97e988e779a3ba54e8a
Jan Hofmann <jan.hofmann87@gmail.com>
2022-08-22 20:45:27 +1000

    Completed feature-hide functionality

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PrintAndHide : MonoBehaviour
 {
-    int i;
+    int i, j;
     public Renderer rend;
     
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        j = Random.Range(200, 251);
+        rend = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -18,5 +20,15 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.tag == "Blue" && i == j)
+        {
+            rend.enabled = false;
+        }
     }
 }
